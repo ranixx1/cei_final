0dcdd80cb83e69015e4227b8efa1785b64982895
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-22 18:16:17 -0300

    criando views de profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class CreateUser(CreateView):
+    template_name = "register.html"
+    model = User
+    fields = ['username', 'email', 'password']
