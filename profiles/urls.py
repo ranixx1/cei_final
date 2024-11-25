872f98d0e11d3e2f432f7fcf8fab4758e53f862d
izacnascimento <izac.n@escolar.ifrn.edu.br>
2024-11-25 15:45:42 -0300

    Configurando rotas de profile e ajuste no template

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import CreateUser
+from .views import RegisterView
+
 
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'registration/login.html'), name = "login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('registrar/', CreateUser.as_view(template_name = 'registration/register.html'), name = "register"),
+    path('registrar/', RegisterView.as_view(template_name = 'registration/register.html'), name = "register"),
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,18 @@
-from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 
-class CreateUser(CreateView):
-    template_name = "register.html"
-    model = User
-    fields = ['username', 'email', 'password']
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+
+    def form_valid(self, form):
+        # Salva o usuário se o formulário for válido
+        form.save()
+        return super().form_valid(form)
+
