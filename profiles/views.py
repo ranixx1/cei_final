df83fcce320a733b599b3b4743012cd6c81407a2
Manoel Cândido <candidomanoel78@gmail.com>
2024-12-06 10:29:25 -0300

    criando view createuser

--- profiles/views.py
+++ profiles/views.py
@@ -1,18 +1,28 @@
-from django.views.generic.edit import FormView
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
+from .forms import UsuarioForm
+from django.shortcuts import get_object_or_404
 
 
-
-
-class RegisterView(FormView):
-    template_name = 'register.html'
-    form_class = UserCreationForm
+class CreateUser(CreateView):
+    template_name = "register/register.html"
+    form_class = UsuarioForm
     success_url = reverse_lazy('login')
 
-
     def form_valid(self, form):
-        # Salva o usuário se o formulário for válido
-        form.save()
-        return super().form_valid(form)
+        turma = form.cleaned_data['turma']
+        group_name = f"{turma.nome}_{turma.serie}_{turma.turno}_{turma.curso}"
+        group_turma = get_object_or_404(Group, name=group_name)
+        grupo = get_object_or_404(Group, name="Discente")
+        url = super().form_valid(form)
+        self.object.groups.add(group_turma)
+        self.object.groups.add(grupo)
+        self.object.save()
+        return url
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = "Registro de novo usuário"
+        context['botão'] = "Cadastrar"
+        return context
