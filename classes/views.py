5a41a287cbbec15dd736aa8b483cd1add6b6a953
manoel <candidomanoel78@gmail.com>
2024-12-08 14:37:37 -0300

    continuando a criar views

--- classes/views.py
+++ classes/views.py
@@ -45,3 +45,41 @@ class ListaTurmas(ListView):
         context = super().get_context_data(**kwargs)
         context['is_admin'] = self.request.user.groups.filter(name="Administrador").exists()
         return context
+
+class TurmaDeleteView(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador"
+    login_url = reverse_lazy('login')
+    model = Turma
+    template_name = 'classes/deletar_turma.html'
+    success_url = reverse_lazy('lista-turmas')
+
+    def post(self, request, *args, **kwargs):
+        turma = self.get_object()
+
+        error_message = turma.can_delete()
+        if error_message:
+            messages.error(self.request, error_message)
+            return self.get(self.request, *args, **kwargs)
+
+        group_name = f"{turma.nome}_{turma.serie}_{turma.turno}_{turma.curso}"
+        group = Group.objects.filter(name=group_name).first()
+        if group:
+            group.delete()
+
+        messages.success(self.request, "Turma excluída com sucesso!")
+        return super().delete(request, *args, **kwargs)
+    
+class TurmaMembersView(DetailView):
+    model = Turma
+    template_name = 'classes/listar_membros.html'
+    context_object_name = 'turma'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_servidor'] = self.request.user.groups.filter(name="Administrador").exists() or self.request.user.groups.filter(name="Docente").exists()
+        context['is_admin'] = self.request.user.groups.filter(name="Administrador").exists()
+        turma = self.object
+        group_name = f"{turma.nome}_{turma.serie}_{turma.turno}_{turma.curso}"
+        group = Group.objects.filter(name=group_name).first()
+        context['members'] = group.user_set.all() if group else []
+        return context
