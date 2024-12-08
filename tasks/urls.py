de5296d6056576c5f05d656ad2bfcbeb0329ab9b
manoel <candidomanoel78@gmail.com>
2024-12-08 16:35:57 -0300

    adicionando rotas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-from .views import TaskListView, TaskDetailView, TaskEventsView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskEventsView, CalendarView, EventCountView, ChartYear
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('calendar/', views.calendar, name='calendar'),
+    path('dashboard/', EventCountView.as_view(), name='dashboard'),
+    path('', CalendarView.as_view(), name='calendar'),
     path('list/', TaskListView.as_view(), name='task-list'),
-    path('task/<int:pk>/', TaskDetailView.as_view(), name='task-view'),  # Use pk para ID
-    path('api/tasks/', TaskEventsView.as_view(), name='task_events'),  # Adiciona a nova view
+    path('chart-year/', ChartYear.as_view(), name='chart-year'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task-view'),
+    path('newtask/', TaskCreateView.as_view(), name='new-task'),
+    path('list/edit/<int:pk>/', TaskUpdateView.as_view(), name='edit-task'),
+    path('list/delete/<int:pk>/', TaskDeleteView.as_view(), name='delete-task'),
+    path('api/tasks/', TaskEventsView.as_view(), name='task_events'),
 ]
