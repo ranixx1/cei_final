ed2ae98ecf97ac0caf7474e5f9b993e05ad2e2e5
manoel <candidomanoel78@gmail.com>
2024-12-08 16:26:42 -0300

    atualizando forms

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,31 +6,30 @@ from django import forms
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ["title", "description", "start_date", "end_date", "start_time", "end_time"]
+        fields = ["title", "description", "turma", "start_date", "end_date", "start_time", "end_time"]
+
         widgets = {
             "title": forms.TextInput(
                 attrs={"class": "form-control", "placeholder": "Insira o título do evento"}
             ),
             "description": forms.Textarea(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Insira a descrição do evento",
-                }
+                attrs={"class": "form-control", "placeholder": "Insira a descrição do evento", "rows": 3 }
             ),
-            "start_date": DateInput(
-                attrs={"class": "form-control"},
+            "turma": forms.Select(
+                attrs={"class": "form-select", "aria-label": "Selecionar turma"}
             ),
-            "end_date": DateInput(
-                attrs={"class": "form-control"},
-            ),
-            "start_time": DateInput(
-                attrs={"class": "form-control"},
-            ),
-            "end_time": DateInput(
-                attrs={"class": "form-control"},
-            ),
+            "start_date": DateInput(attrs={"class": "form-control", "placeholder": "dd/mm/aaaa"}),
+            "end_date": DateInput(attrs={"class": "form-control", "placeholder": "dd/mm/aaaa"}),
+            "start_time": DateInput(attrs={"class": "form-control", "placeholder": "hh:mm:ss"}),
+            "end_time": DateInput(attrs={"class": "form-control", "placeholder": "hh:mm:ss"}),
         }
-        exclude = ["user"]
 
     def __init__(self, *args, **kwargs):
         super(TaskForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        task = super().save(commit=False)
+        task.id_turma = self.cleaned_data['turma'].id
+        if commit:
+            task.save()
+        return task
